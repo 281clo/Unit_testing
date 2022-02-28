08896d07da8c7d8a26456ecfb7ce47f3fbbd1044
Carlos McCrum <69286570+281clo@users.noreply.github.com>
2022-02-28 12:46:41 -0800

    Uploading Pytest files

---
+++ test_First.py
@@ -0,0 +1,21 @@
+'''pytest
+test1
+test_1
+test_1
+1_test
+pytest will run all files from current dir/ subdirec test_*.py or *_test.py'''
+
+def test_1():
+    x=10
+    y=10
+    assert x==y
+
+def test_2():
+    name="Selenium"
+    title="Selenium is for web automation"
+    assert name in title
+
+def test_3():
+    name="Jenkins"
+    title="Jenkins is CI server"
+    assert name is title , "Title does not match"
